d55666c271273a1692544ab718a48d59bc4c9697
yenthph31575 <yenthph31575@fpt.edu.vn>
2025-11-18 09:36:10 +0700

    updated

--- src/api/banner/queries.ts
+++ src/api/banner/queries.ts
@@ -1,7 +1,7 @@
-import { createQuery, createMutation } from 'react-query-kit';
-import { getBanners, getBannerById, createBanner, updateBanner, deleteBanner } from './requests';
-import type { IBannerResponse, IBanner, IBannerQuery } from './types';
 import type { BannerSchema } from '@/modules/BannerManagementPage/libs/validators';
+import { createMutation, createQuery } from 'react-query-kit';
+import { createBanner, deleteBanner, getBannerById, getBanners, updateBanner } from './requests';
+import type { IBanner, IBannerQuery, IBannerResponse } from './types';
 
 export const useBanners = createQuery<IBannerResponse, Partial<IBannerQuery>>({
   queryKey: ['admin/banners'],
@@ -12,7 +12,6 @@ export const useBannerByIdQuery = createQuery<IBanner, string>({
   queryKey: ['banner'],
   fetcher: (id) => getBannerById(id),
 });
-
 export const useCreateBannerMutation = createMutation<IBanner, BannerSchema>({
   mutationFn: (data) => createBanner(data),
 });
